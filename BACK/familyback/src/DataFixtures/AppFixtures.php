8fd65574b19023573b8a63fc772be5d3cc2c6cd3
SolenneMZ <smazabraud@gmail.com>
2019-07-24 12:30:30 +0200

    fake data for one user (test)

--- BACK/familyback/src/DataFixtures/AppFixtures.php
+++ BACK/familyback/src/DataFixtures/AppFixtures.php
@@ -3,7 +3,8 @@
 /*
     File used to provide fake data 
 
-    Fake data are generate thanks to Faker (PHP library)
+    Fake data are generate thanks to Faker (PHP library) 
+    and our DataProvider (App\DataFixtures\Faker\DataProvider)
 
     https://github.com/fzaninotto/Faker 
 
@@ -14,36 +15,69 @@ namespace App\DataFixtures;
 use Faker;
 use Faker\Factory;
 
+use App\Entity\User;
+use App\DataFixtures\Faker\DataProvider;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use App\DataFixtures\Faker\DataProvider;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class AppFixtures extends Fixture
 {
+
+    // to encode the plain password
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder) 
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+
     public function load(ObjectManager $manager)
     {
         $generator = Factory::create('fr_FR');
 
-        // App\DataFixtures\Faker\DataProvider
+        // App\DataFixtures\Faker\DataProvider - file with fake data
         $generator->addProvider(new DataProvider($generator));
 
-        //$manager->flush();
+        $populator = new Faker\ORm\Doctrine\Populator($generator, $manager);
 
-        // TODO: User
+        // ***** User *****
+            $user = new User(); 
+            $user->setFirstname('Barbara');
+            $user->setLastname('Goulde');
+            $user->setbirthDate(); // TODO: 
+            $user->setEmail('barbara.goulde@fake.mail');
+            $time = new \Datetime;
+            $user-setCreatedAt($time);
 
+            /* 
+                'barbiche' is the password of our fake $user
+                encode password brings security to our app 
+                there is no need to display a plain password
+            */
+            $encodedPassword = $this->passwordEncoder->encodePassword($user, 'barbiche');
+            $user->setPassword($encodedPassword);
+
+            $manager->persist($user);
+
+            
+            
         // TODO: Event
-
+        
         // TODO: Group
-
-        // TODO: Catégory
-
+        
+        // TODO: Category
+        
         // TODO: Tag (= family members)
-
+        
         // TODO: Picture
-
+        
         // TODO: Comment
-
+        
         // TODO: Guest
+        
+        $manager->flush();
     }
 }
