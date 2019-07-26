8034e1695607baeda358453539efe2c334cb3764
SolenneMZ <smazabraud@gmail.com>
2019-07-26 10:34:22 +0200

    make:form User (edit + create)

--- BACK/familyback/src/Controller/UserController.php
+++ BACK/familyback/src/Controller/UserController.php
@@ -5,15 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/profile", name="profile_")
+ */
 class UserController extends AbstractController
 {
     /**
-     * @Route("/profile", name="profile")
+     * @Route("/", name="index")
      */
     public function index()
     {
         return $this->render('profile/index.html.twig', [
             'title' => 'Mon profil',
         ]);
+    }
+
+    /**
+     * @Route("/profile/edit", name="edit", methods={"GET", "POST"})
+     */
+    public function edit()
+    {
+        $user = $this->getUser();
+
+        
     }
 }
