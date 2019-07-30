f83dedb791cfcce3068f3e4d7be6f67ba2a7b438
SolenneMZ <smazabraud@gmail.com>
2019-07-30 10:15:20 +0200

    relation comment - user

--- BACK/familyback/src/Entity/User.php
+++ BACK/familyback/src/Entity/User.php
@@ -59,9 +59,15 @@ class User
      */
     private $tribe;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="relation")
+     */
+    private $comments;
+
     public function __construct()
     {
         $this->events = new ArrayCollection();
+        $this->comments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -180,6 +186,37 @@ class User
     public function setTribe(?Tribe $tribe): self
     {
         $this->tribe = $tribe;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setRelation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getRelation() === $this) {
+                $comment->setRelation(null);
+            }
+        }
 
         return $this;
     }
