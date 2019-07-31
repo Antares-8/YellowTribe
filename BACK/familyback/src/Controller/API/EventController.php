814dff1acebb5adee139d893272dd0c6ebce6ed1
Antares-8 <flo-qumal@hotmail.fr>
2019-07-31 14:12:32 +0200

    Co-authored-by: SolenneMZ <smazabraud@gmail.com>delete Fos rest bundle and  JMS serialiser

--- BACK/familyback/src/Controller/API/EventController.php
+++ BACK/familyback/src/Controller/API/EventController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\API;
 
+use App\Entity\Event;
 use App\Repository\EventRepository;
+use JMS\Serializer\SerializerBuilder;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -13,13 +16,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class EventController extends AbstractController
 {
 
-    /**
-     * @Route("/events", name="events_list")
-     */
-    public function list(EventRepository $repository)
-    {
-        $events = $repository->findAll();
+    // /**
+    //  * @Route("/events", name="events_list")
+    //  */
+    // public function list(EventRepository $repository)
+    // {
+    //     $events = $repository->findAll();
 
-        return $this->json($events); // Convert the content-type into json instead of HTML
+    //     return $this->json($events); // Convert the content-type into json instead of HTML
+    // }
+
+
+    /**
+     * @Route("/event", name="eventList")
+     */
+    public function eventList(EventRepository $repository)
+    {
+        $serializer = SerializerBuilder::create()->build();
+        $serializer->serialize($repository, 'json');
+
+        dd($repository);
+        return $repository;
+
+
+        // $data = $serializer->deserialize($inputStr, $typeName, $format);
+
+        // return $this->render('event/index.html.twig', [
+        //     'title' => 'Calendrier',
+        // ]);
+
     }
 }
