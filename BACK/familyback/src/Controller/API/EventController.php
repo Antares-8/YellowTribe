daa3d41e27dc56fc80559098b9eeded8adc4162d
SolenneMZ <smazabraud@gmail.com>
2019-08-01 13:34:45 +0200

    clear Controller

--- BACK/familyback/src/Controller/API/EventController.php
+++ BACK/familyback/src/Controller/API/EventController.php
@@ -28,49 +28,8 @@ class EventController extends AbstractController
     {
         $events = $repository->findByTitle();
 
-        //dd($events);
-
-        // $normalizers = [
-        //     new ObjectNormalizer(),
-        // ];
-
-        // $encoders = [
-        //     new JsonEncoder(),
-        // ];
-
-        // $serializer = new Serializer($normalizers, $encoders);
-
-        // $serializedData = $serializer->serialize($events, 'json', [
-        //     ObjectNormalizer::SKIP_NULL_VALUES => true,
-        // ]);
-
-        // dd($serializedData);
-
-        // return $this->json($events, Response::HTTP_OK, [], [
-        //     ObjectNormalizer::SKIP_NULL_VALUES => true,
-        // ]); // Convert the content-type into json instead of HTML
-
         return $this->json($events);
     }
-
-
-    // /**
-    //  * @Route("/event", name="eventList")
-    //  */
-    // public function eventList(EventRepository $repository)
-    // {
-    //     $serializer = SerializerBuilder::create()->build();
-    //     $serializer->serialize($repository, 'json');
-
-    //     dd($repository);
-    //     return $repository;
-
-
-        // $data = $serializer->deserialize($inputStr, $typeName, $format);
-
-        // return $this->render('event/index.html.twig', [
-        //     'title' => 'Calendrier',
-        // ]);
 
     
 }
